cbfd7d70d655d7412f16f69a9fba8dbd762d33e7
Itacir Gabral <itacirgabral@oniq.com.br>
2020-04-20 10:29:35 -0400

    sqlapi

---
+++ dao/SQL.js
@@ -0,0 +1,7 @@
+module.exports = {
+  novoLembrete: 'INSERT INTO devmt.todo (lembrete) VALUES (?);',
+  faltaFazer: 'SELECT * FROM devmt.todo WHERE concluido=0;',
+  concluir: 'UPDATE devmt.todo SET concluido=1 WHERE id=?;',
+  esquecer: 'DELETE FROM devmt.todo WHERE id=? AND concluido=1;',
+  limparTudo: 'DELETE FROM devmt.todo;',
+}
--- dao/dao.spec.js
+++ dao/dao.spec.js
@@ -2,29 +2,41 @@ const test = require('ava')
 const mariadb = require('mariadb')
 
 const mariaConf = require('./mariaConf')
+const SQL = require('./SQL')
 
 test.before('Criando o pool de conexões', t => {
   const pool = mariadb.createPool(mariaConf)
   t.context.pool = pool
 })
 
-test.serial('ping', async t => {
+test.serial('novoLembrete', async t => {
   const conn = await t.context.pool.getConnection()
 
-  await conn.ping()
-  t.pass()
+  const resultado = await conn.query(SQL.novoLembrete, "mais um lembrete")
 
+  t.falsy(resultado)
+  
   conn.release()
-});
+})
 
-test.serial('novoLembrete', async t => {
-  const conn = await t.context.pool.getConnection();
+test.serial('faltaFazer', async t => {
+  const conn = await t.context.pool.getConnection()
 
-  const resultado = await conn.query('INSERT INTO devmt.todo (lembrete) VALUES ("mais um lembrete");')
+  const resultado = await conn.query('SELECT * FROM devmt.todo WHERE concluido = 0;')
+
+  return t.falsy(resultado)
+})
+
+test.serial('concluir', async t => {
+  const conn = await t.context.pool.getConnection()
+
+  const resultado = await conn.query(SQL.concluir, 8)
 
   return t.falsy(resultado)
 })
 
 test.after('Desligando conexões do banco', async t => {
+  const conn = await t.context.pool.getConnection()
+  await conn.query(SQL.limparTudo)
   await t.context.pool.end()
 })
